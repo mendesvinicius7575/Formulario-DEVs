d677c400050c066a98b283296948fe877de83876
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-10 10:25:40 -0300

    Pronto

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,12 @@
 describe('Form Devs', () => {
+  before(() => {
+    
+  }) 
   beforeEach(() => cy.visit('../../formulario.html'))
   it.only('fills the form and submit', () => {
     cy.get('#titulo').should('be.visible').and('have.text', 'Cadastro de DEVs')
     cy.get('#subtitulo').should('have.text', 'Complete suas informações')
+    cy.get('#fundo-branco').prev().screenshot('header')
   })
   
 
