6241e5f53c83ce433ffd558d12ce3c6fd456b158
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-10 09:07:17 -0300

    Custom Commands

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,33 +1,16 @@
 describe('Form Devs', () => {
   beforeEach(() => cy.visit('../../formulario.html'))
   it('fills the form and submit', () => {
-    cy.get('#nome').as('name').type('Vinicius')
-    cy.get('#sobrenome').as('lastname').type('Mendes')
-    cy.get('#email').as('email').type('olaolaola@gmail.com')
-    cy.get('input[type="radio"][value="fullstack"]').as('fullstackradio').check()
-    cy.get('#senioridade').as('seniority').select('Sênior')
-    cy.get('input[type="checkbox"][value="HTML"]').as('htmlCheckbox').check()
-    cy.get('input[type="checkbox"][value="CSS"]').as('cssCheckbox').check()
-    cy.get('input[type="checkbox"][value="Javascript"]').as('jsCheckbox').check()
-
-    cy.get('.botao').click()
-
-    cy.get('@name').should('be.empty')
-    cy.get('@lastname').should('be.empty')
-    cy.get('@email').should('be.empty')
-
-    cy.get('@fullstackradio').should('not.be.checked')
-    cy.get('input[type="radio"][value="frontend"]').should('be.checked')
-    cy.get('@seniority').find('option').contains('Selecione').should('be.selected')
-    cy.get('input[type="checkbox"][value="PHP"]').should('not.be.checked')
-    cy.get('input[type="checkbox"][value="C#"]').should('not.be.checked')
-    cy.get('input[type="checkbox"][value="Python"]').should('not.be.checked')
-    cy.get('input[type="checkbox"][value="Java"]').should('not.be.checked')
+    
   })
+  
+
+    it.only('Comando Inserção', () =>{
+      cy.fillformSubmit();
+      cy.assertForm();
+    })
+
   it('fills the form and submit', () => {
- 
-  })
-  it.only('fills the form and submit', () => {
     cy.get('#nome').as('nameOne').type('Vinicius')
 
     cy.get('.botao').click()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,29 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillformSubmit', () => {
+    cy.get('#nome').as('name').type('Vinicius')
+    cy.get('#sobrenome').as('lastname').type('Mendes')
+    cy.get('#email').as('email').type('olaolaola@gmail.com')
+    cy.get('input[type="radio"][value="fullstack"]').as('fullstackradio').check()
+    cy.get('#senioridade').as('seniority').select('Sênior')
+    cy.get('input[type="checkbox"][value="HTML"]').as('htmlCheckbox').check()
+    cy.get('input[type="checkbox"][value="CSS"]').as('cssCheckbox').check()
+    cy.get('input[type="checkbox"][value="Javascript"]').as('jsCheckbox').check()
+
+    cy.get('.botao').click()
+})
+
+Cypress.Commands.add('assertForm', () => {
+    cy.get('@name').should('be.empty')
+    cy.get('@lastname').should('be.empty')
+    cy.get('@email').should('be.empty')
+
+    cy.get('@fullstackradio').should('not.be.checked')
+    cy.get('input[type="radio"][value="frontend"]').should('be.checked')
+    cy.get('@seniority').find('option').contains('Selecione').should('be.selected')
+    cy.get('input[type="checkbox"][value="PHP"]').should('not.be.checked')
+    cy.get('input[type="checkbox"][value="C#"]').should('not.be.checked')
+    cy.get('input[type="checkbox"][value="Python"]').should('not.be.checked')
+    cy.get('input[type="checkbox"][value="Java"]').should('not.be.checked')
+})
+
+
+
